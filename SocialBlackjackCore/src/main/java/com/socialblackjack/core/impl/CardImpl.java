4e5243a83b6aaae05f38e1dab564d79220ef64d3
Javier Molina Alonso <javier.molina@pay-touch.comzzzzzz>
2015-07-18 19:18:01 +0200

    Improved sbj core

--- SocialBlackjackCore/src/main/java/com/socialblackjack/core/impl/CardImpl.java
+++ SocialBlackjackCore/src/main/java/com/socialblackjack/core/impl/CardImpl.java
@@ -6,11 +6,22 @@ import com.socialblackjack.core.exceptions.InvalidRankException;
 
 public class CardImpl implements Card {
 
-	private byte card;
+	public static final int CARDS_PER_COLOR = 13;
+	public static final int AMOUNT_COLORS = 4;
+
+	private final byte card;
 	
 	public CardImpl(char rank, char color){
-		card = 0;
+		card = getCardFromRankAndColor(rank, color);
+	}
+
+    public CardImpl(byte card){
+        this.card = card;
+    }
+
+	private byte getCardFromRankAndColor(char rank, char color) {
 		boolean found = false;
+		byte card = 0;
 		for(int i = 0; i < COLORS.length; i++){
 			if(COLORS[i] == color){
 				card = (byte) (CARD_PER_COLOR * i);
@@ -30,31 +41,28 @@ public class CardImpl implements Card {
 		if(!found){
 			throw new InvalidColorException();
 		}
+        return card;
 	}
-	
-	public CardImpl(byte card){
-		this.card = card;
-	}
-	
+
 	public byte getCard() {
 		return card;
 	}
 
 	public Integer getRankValue() {
-		return card % 13;
+		return card % CARDS_PER_COLOR;
 	}
 
 	public String getRank() {
-		return String.valueOf(RANKS[card % 13]);
+		return String.valueOf(RANKS[card % CARDS_PER_COLOR]);
 	}
 
 	public String getColor() {
-		return String.valueOf(COLORS[card / 13 % 4]);
+		return String.valueOf(COLORS[card / CARDS_PER_COLOR % AMOUNT_COLORS]);
 	}
 
 	public String getValue() {
 		StringBuilder sb = new StringBuilder();
-		sb.append(RANKS[card % 13]).append(COLORS[card / 13 % 4]);
+		sb.append(RANKS[card % 13]).append(COLORS[card / CARDS_PER_COLOR % AMOUNT_COLORS]);
 		return sb.toString();
 	}
 
