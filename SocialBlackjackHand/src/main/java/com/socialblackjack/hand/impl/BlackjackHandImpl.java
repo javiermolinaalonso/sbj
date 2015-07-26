32459a2475dceb175f103db2df916ddb2c61f84b
Javier Molina Alonso <javier.molina@pay-touch.comzzzzzz>
2015-07-26 23:17:16 +0200

    Improved aces calculus

--- SocialBlackjackHand/src/main/java/com/socialblackjack/hand/impl/BlackjackHandImpl.java
+++ SocialBlackjackHand/src/main/java/com/socialblackjack/hand/impl/BlackjackHandImpl.java
@@ -11,39 +11,43 @@ import java.util.stream.Collectors;
 
 /**
  * This is the basic implementation of a blackjack hand
- *
+ * <p>
  * The constructor
  */
 public abstract class BlackjackHandImpl implements BlackjackHand {
 
-	protected List<Card> cards;
-	
-	public BlackjackHandImpl() {
-		this.cards = new ArrayList<>();
-	}
+    protected List<Card> cards;
 
-	public void addCard(Card card) throws MaximumCardsReached {
-		if(cards.size() == MAX_CARDS)
-			throw new MaximumCardsReached();
-		cards.add(card);
-	}
+    public BlackjackHandImpl() {
+        this.cards = new ArrayList<>();
+    }
 
-	public Integer getHigherValue() {
-		Integer max = 0;
-		for(Integer v : getValues()){
-			max = Math.max(max, v);
-		}
-		return max;
-	}
+    public void addCard(Card card) throws MaximumCardsReached {
+        if (cards.size() == MAX_CARDS)
+            throw new MaximumCardsReached();
+        cards.add(card);
+    }
 
-	public List<Integer> getValues() {
-        if(containsAnAce()) {
+    public Integer getHigherValue() {
+        Integer max = 0;
+        for (Integer v : getValues()) {
+            max = Math.max(max, v);
+        }
+        return max;
+    }
+
+    public List<Integer> getValues() {
+        if (containsAnAce()) {
             Set<Integer> result = new HashSet<>();
-            List<List<Integer>> acesList = cards.stream().filter(x -> Rank.ACE.equals(x.getRank())).map(x -> HandUtils.getValues(x.getRank())).collect(Collectors.toList());
+            int aces = (int)cards.stream().filter(x -> Rank.ACE.equals(x.getRank())).count();
             Integer resultWithoutAces = cards.stream().filter(x -> !Rank.ACE.equals(x.getRank())).map(x -> HandUtils.getValue(x.getRank())).reduce((x, y) -> x + y).get();
-            for (List<Integer> integers : acesList) {
-                for (Integer integer : integers) {
-                    result.add(integer + resultWithoutAces);
+
+            if(aces == 0) {
+                result.add(resultWithoutAces);
+            } else {
+                for(int i = 0; i < aces; i++) {
+                    result.add(resultWithoutAces + aces);
+                    result.add(resultWithoutAces + aces + 10);
                 }
             }
             return result.stream().filter(x -> x <= MAX_VALUE).sorted().collect(Collectors.toList());
@@ -53,48 +57,27 @@ public abstract class BlackjackHandImpl implements BlackjackHand {
             result.add(value);
             return result;
         }
-//		LinkedList<Integer> currentValidResults = new LinkedList<>();
-//		currentValidResults.add(0);
-//		for(Card card : cards){
-//			List<Integer> auxList = new ArrayList<>(currentValidResults);
-//			currentValidResults.clear();
-//			for(Integer curValue : HandUtils.getValues(card.getRank())){
-//				for(Integer acumValue : auxList){
-//					currentValidResults.add(acumValue+curValue);
-//				}
-//			}
-//		}
-//		Iterator<Integer> it = currentValidResults.iterator();
-//		while(it.hasNext()){
-//			if(currentValidResults.size() == 1)
-//				break;
-//			if(it.next() > MAX_VALUE){
-//				it.remove();
-//			}
-//		}
-//        currentValidResults.sort(Integer::compareTo);
-//		return currentValidResults;
-	}
+    }
 
     private boolean containsAnAce() {
         return cards.stream().anyMatch(x -> Rank.ACE.equals(x.getRank()));
     }
 
     public boolean isBlackjack() {
-		return haveTwoCards() && MAX_VALUE.equals(getHigherValue());
-	}
+        return haveTwoCards() && MAX_VALUE.equals(getHigherValue());
+    }
 
-	public boolean isBust() {
-		return getHigherValue() > MAX_VALUE;
-	}
+    public boolean isBust() {
+        return getHigherValue() > MAX_VALUE;
+    }
 
-	public boolean isSoft() {
+    public boolean isSoft() {
         return Rank.ACE.equals(cards.get(0).getRank())
                 || Rank.ACE.equals(cards.get(1).getRank());
-	}
+    }
 
     protected boolean haveTwoCards() {
-        return cards.size() == 2 ;
+        return cards.size() == 2;
     }
 
     public abstract Collection<Card> getShowableCards();
